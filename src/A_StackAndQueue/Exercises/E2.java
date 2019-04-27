a610eeac4ce9cd682fd70566af0db3e463ea1efe
Deianov <deqnow@gmail.com>
2019-04-27 11:45:38 +0200

    Stacks and Queues - Exercises

---
+++ src/A_StackAndQueue/Exercises/E2.java
@@ -0,0 +1,35 @@
+//2.Basic Stack Operations
+package A_StackAndQueue.Exercises;
+
+import java.util.*;
+
+public class E2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] commands = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        int[] data = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < commands[0] ; i++) {
+            stack.push(data[i]);
+        }
+
+        for (int i = 0; i < commands[1] ; i++) {
+            if (!stack.isEmpty())
+                stack.pop();
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        if (stack.stream().anyMatch(e -> e.equals(commands[2]))) {
+            System.out.println("true");
+        } else {
+            System.out.println(Collections.min(stack));
+        }
+    }
+}
