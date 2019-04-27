d94b9b8a49735dd01ad6bfddf28603d485ddef79
Ognian <deqnow@gmail.com>
2019-04-27 23:47:29 +0200

    Stack and Queue - Exercises

---
+++ src/A_StackAndQueue/Exercises/E6.java
@@ -0,0 +1,48 @@
+//06.Balanced Parentheses
+package A_StackAndQueue.Exercises;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class E6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Deque<Character> chars = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            chars.add(c);
+        }
+
+        Deque<Character> temp = new ArrayDeque<>();
+        Character a = null;
+        Character b = null;
+
+        while (!chars.isEmpty()) {
+
+            int lastSize = chars.size();
+
+            while (!chars.isEmpty()) {
+                a = chars.poll();
+                b = chars.peek();
+                if ((a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}')) {
+                    chars.poll();
+                } else {
+                    temp.offer(a);
+                }
+            }
+
+            while (!temp.isEmpty()) {
+                chars.add(temp.pop());
+            }
+
+            if (chars.size() == lastSize) {
+                break;
+            }
+        }
+
+        System.out.println(chars.isEmpty() ? "YES" : "NO");
+    }
+}
