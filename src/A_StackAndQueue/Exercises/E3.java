8961c6a802e9ecf90caa56535ec678e2f2796d48
Deianov <deqnow@gmail.com>
2019-04-27 12:14:11 +0200

    Stacks and Queues - Exercises

---
+++ src/A_StackAndQueue/Exercises/E3.java
@@ -0,0 +1,31 @@
+//3.Maximum Element
+package A_StackAndQueue.Exercises;
+
+import java.util.*;
+
+public class E3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        while (n-- > 0) {
+
+            int[] command = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+
+            switch (command[0]) {
+                case 1:
+                    stack.push(command[1]);
+                    break;
+                case 2:
+                    stack.poll();
+                    break;
+                case 3:
+                    int maxValue = stack.isEmpty() ? 0 : Collections.max(stack);
+                    System.out.println(maxValue);
+                    break;
+            }
+        }
+    }
+}
