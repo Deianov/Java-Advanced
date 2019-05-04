9e57f657b5b9b527604ca837d00494d623d5f2ba
Ognian <deqnow@gmail.com>
2019-05-04 10:36:47 +0200

    Multidimensional Arrays - Lab

---
+++ src/B_MultidimensionalArrays/Lab/L02.java
@@ -0,0 +1,35 @@
+//02. Positions Of
+package B_MultidimensionalArrays.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        scanner.nextLine();
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < rows ; row++) {
+            matrix[row] = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        int number = scanner.nextInt();
+        boolean isFound = false;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (matrix[row][col] == number) {
+                    System.out.println(row + " " + col);
+                    isFound = true;
+                }
+            }
+        }
+
+        if (!isFound) System.out.println("not found");
+    }
+}
---
+++ src/B_MultidimensionalArrays/Lab/L03.java
@@ -0,0 +1,11 @@
+//03. Intersection of Two Matrices
+package B_MultidimensionalArrays.Lab;
+
+import java.util.Scanner;
+
+public class L03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
