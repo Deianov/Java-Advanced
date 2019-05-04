445172af355c9745aa4543e89bedf5f630393e34
Ognian <deqnow@gmail.com>
2019-05-04 14:15:34 +0200

    Multidimensional Arrays - Lab

---
+++ src/B_MultidimensionalArrays/Lab/L06.java
@@ -0,0 +1,31 @@
+//06. Print Diagonals of Square Matrix
+package B_MultidimensionalArrays.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+
+        int[][] matrix = new int[size][];
+
+        for (int row = 0; row < size; row++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            matrix[row] = arr;
+        }
+
+        int col = 0;
+        for (int row = 0; row < size; row++) {
+            System.out.print(matrix[row][col++] + " ");
+        }
+        System.out.print(System.lineSeparator());
+
+        col = 0;
+        for (int row = size - 1; row >= 0 ; row--) {
+            System.out.print(matrix[row][col++] + " ");
+        }
+    }
+}
