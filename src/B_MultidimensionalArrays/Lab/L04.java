9af018ccdaaabf47a3d3eba6a34741ae35488826
Ognian <deqnow@gmail.com>
2019-05-04 12:59:31 +0200

    Multidimensional Arrays - Lab

---
+++ src/B_MultidimensionalArrays/Lab/L04.java
@@ -0,0 +1,29 @@
+//04. Sum Matrix Elements
+package B_MultidimensionalArrays.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] line = scanner.nextLine().split(", ");
+        int rows = Integer.parseInt(line[0]);
+        int cols = Integer.parseInt(line[1]);
+
+        int[][] matrix = new int[rows][cols];
+        int sum = 0;
+
+        for (int row = 0; row < rows; row++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+            matrix[row] = arr;
+        }
+
+        for (int row = 0; row < rows; row++) {
+            sum += Arrays.stream(matrix[row]).sum();
+        }
+
+        System.out.printf("%d%n%d%n%d",rows, cols, sum);
+    }
+}
