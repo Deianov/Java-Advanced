a2aed1a205b058a6a2b651f67d80e8c285e14f0c
Ognian <deqnow@gmail.com>
2019-05-04 12:15:01 +0200

    Multidimensional Arrays - Lab

--- src/B_MultidimensionalArrays/Lab/L03.java
+++ src/B_MultidimensionalArrays/Lab/L03.java
@@ -4,8 +4,44 @@ package B_MultidimensionalArrays.Lab;
 import java.util.Scanner;
 
 public class L03 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+
+        int rows = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
+
+        char[][] firstMatrix = readMatrixChar(rows, cols);
+        char[][] secondMatrix = readMatrixChar(rows, cols);
+
+        for (int row = 0; row < rows; row++) {
+
+            for (int col = 0; col < cols; col++) {
+                if (firstMatrix[row][col] == secondMatrix[row][col]) {
+                    System.out.print(firstMatrix[row][col] + " ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+
+            System.out.print(System.lineSeparator());
+        }
+    }
+
+    private static char[][] readMatrixChar(int rows, int cols) {
+
+        char[][] matrix = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            String[] line = scanner.nextLine().split("\\s");
+
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = line[col].charAt(0);
+            }
+        }
+
+        return matrix;
     }
 }
