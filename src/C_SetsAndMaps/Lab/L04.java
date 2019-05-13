5d770b6b9ba781bbe10b4c968fce0eca6ca32896
Deianov <deqnow@gmail.com>
2019-05-13 22:32:11 +0200

    Sets and Maps - Lab

--- src/C_SetsAndMaps/Lab/L04.java
+++ src/C_SetsAndMaps/Lab/L04.java
@@ -1,5 +1,24 @@
 //04. Count Real Numbers
 package C_SetsAndMaps.Lab;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class L04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToDouble(Double::parseDouble).toArray();
+
+        Map<Double, Integer> valuesWithOccurrences = new LinkedHashMap<>();
+
+        for (double num : numbers) {
+            valuesWithOccurrences.putIfAbsent(num, 0);
+            valuesWithOccurrences.put(num, valuesWithOccurrences.get(num) + 1);
+        }
+
+        valuesWithOccurrences.forEach((key, val) -> System.out.printf("%.1f -> %d%n", key, val));
+    }
 }
