c54b7ce62d060150e9876ba47112f2bc6e4589dc
Deianov <deqnow@gmail.com>
2019-05-14 20:19:25 +0200

    Sets and Maps - Lab

---
+++ src/C_SetsAndMaps/Lab/L06.java
@@ -0,0 +1,32 @@
+//06. Product Shop
+package C_SetsAndMaps.Lab;
+
+        import java.util.LinkedHashMap;
+        import java.util.Scanner;
+        import java.util.TreeMap;
+
+public class L06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        TreeMap<String, LinkedHashMap<String, Double>> shops = new TreeMap<>();
+
+        while (!"Revision".equals(input = scanner.nextLine())) {
+
+            String[] data = input.split(", ");
+            String shopName = data[0];
+            String productName = data[1];
+            double productPrice = Double.parseDouble(data[2]);
+
+            shops.putIfAbsent(shopName, new LinkedHashMap<>());
+            shops.get(shopName).put(productName, productPrice);
+        }
+
+        shops.forEach((key, value) -> {
+            System.out.printf("%s->%n", key);
+            value.entrySet().forEach(e -> System.out.printf("Product: %s, Price: %.1f%n", e.getKey(), e.getValue()));
+        });
+
+    }
+}
