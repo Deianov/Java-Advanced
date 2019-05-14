f3ddac4957d3a6d0aedc808f23ef94f6213dd0d1
Deianov <deqnow@gmail.com>
2019-05-14 22:00:20 +0200

    Sets and Maps - Lab

---
+++ src/C_SetsAndMaps/Lab/L07a.java
@@ -0,0 +1,43 @@
+//07. Cities by Continent and Country
+package C_SetsAndMaps.Lab;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class L07a {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        LinkedHashMap<String, LinkedHashMap<String, List<String>>> continents = new LinkedHashMap<>();
+
+        while (n-- > 0) {
+            String[] data = scanner.nextLine().split("\\s+");
+
+            String continentName = data[0];
+            String countryName = data[1];
+            String cityName = data[2];
+
+            if(!continents.containsKey(continentName)) {
+                continents.put(continentName, new LinkedHashMap<>(){{put(countryName, new ArrayList<>(){{add(cityName);}});}});
+            } else {
+                if (!continents.get(continentName).containsKey(countryName)) {
+                    continents.get(continentName).put(countryName, new ArrayList<>(){{add(cityName);}});
+                } else {
+                    continents.get(continentName).get(countryName).add(cityName);
+                }
+            }
+        }
+
+        continents.forEach((key, value) -> {
+            System.out.printf("%s:%n", key);
+            value.entrySet().forEach(e -> {
+                System.out.printf("\t%s -> %s%n",
+                        e.getKey(),
+                        e.getValue().toString().replaceAll("[\\[\\]]", ""));
+            });
+        });
+    }
+}
