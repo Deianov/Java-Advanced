8d74e97ac3cb49899f7f2b70cc15daaa588d8d94
Deianov <deqnow@gmail.com>
2019-05-16 17:35:49 +0200

    Sets and Maps - Exercises

---
+++ src/C_SetsAndMaps/Exercises/E04.java
@@ -0,0 +1,26 @@
+//04. Count Symbols
+package C_SetsAndMaps.Exercises;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class E04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        TreeMap<Character, Integer> symbols = new TreeMap<>();
+
+        for (char ch: scanner.nextLine().toCharArray()) {
+
+            if (symbols.containsKey(ch)) {
+                symbols.put(ch, symbols.get(ch) + 1);
+            } else {
+                symbols.put(ch, 1);
+            }
+        }
+
+        symbols.forEach((key, value) -> {
+            System.out.printf("%c: %d time/s%n", key, value);
+        });
+    }
+}
