20317ae6cfce23ffdb9b11d98b9961849bf5a696
Deianov <deqnow@gmail.com>
2019-05-16 22:21:39 +0200

    Sets and Maps - Exercises

---
+++ src/C_SetsAndMaps/Exercises/E09.java
@@ -0,0 +1,42 @@
+//09. User Logs
+package C_SetsAndMaps.Exercises;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class E09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        TreeMap<String, LinkedHashMap<String, Integer>> users = new TreeMap<>();
+
+        Pattern pattern = Pattern.compile("^IP=(?<ip>.*) message=.* user=(?<user>.*)$");
+
+        while (!"end".equals(input = scanner.nextLine())) {
+
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                String ip = matcher.group("ip");
+                String user = matcher.group("user");
+
+                users.putIfAbsent(user, new LinkedHashMap<>());
+                users.get(user).putIfAbsent(ip, 0);
+                users.get(user).put(ip, users.get(user).get(ip) + 1);
+            }
+        }
+
+        users.forEach((key, value) -> {
+            System.out.printf("%s:%n", key);
+            int index = 0;
+            for (Map.Entry<String, Integer> entry : value.entrySet()) {
+                System.out.printf("%s%s => %d", index++ == 0 ? "" : ", ", entry.getKey(), entry.getValue());
+            }
+            System.out.printf(".%n");
+        });
+    }
+}
