0eb6b423c49c4e7b674737ce2bac01d1379232fe
Deianov <deqnow@gmail.com>
2019-05-17 09:48:04 +0200

    Sets and Maps - Exercises

---
+++ src/C_SetsAndMaps/Exercises/E11.java
@@ -0,0 +1,39 @@
+//11. Logs Aggregator
+package C_SetsAndMaps.Exercises;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class E11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        TreeMap<String, TreeMap<String, Integer>> logs = new TreeMap<>();
+
+        Pattern pattern = Pattern.compile("(?<ip>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\s+(?<user>[a-zA-Z]+[\\s]*[a-zA-Z]*)\\s+(?<duration>\\d+)");
+
+        while (n-- > 0) {
+
+            Matcher matcher = pattern.matcher(scanner.nextLine());
+
+            if (matcher.find()) {
+
+                String ip = matcher.group("ip");
+                String user = matcher.group("user");
+                int duration = Integer.parseInt(matcher.group("duration"));
+
+                logs.putIfAbsent(user, new TreeMap<>());
+                logs.get(user).putIfAbsent(ip, 0);
+                logs.get(user).put(ip, logs.get(user).get(ip) + duration);
+            }
+        }
+
+        logs.forEach((key, value) -> {
+            int duration = value.values().stream().mapToInt(Integer::intValue).sum();
+            String ips = value.keySet().toString();
+            System.out.printf("%s: %d %s%n",key, duration, ips);
+        });
+    }
+}
