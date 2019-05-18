2a45297ceb589d7086d55e05e612991ee0a022eb
Deianov <deqnow@gmail.com>
2019-05-18 15:49:28 +0200

    Streams, Files and Directories - Lab

--- src/D_StreamsFilesAndDirectories/Lab/L07.java
+++ src/D_StreamsFilesAndDirectories/Lab/L07.java
@@ -1,8 +1,27 @@
 //07. List Files
 package D_StreamsFilesAndDirectories.Lab;
 
+import java.io.File;
+
 class L07 {
     public static void main(String[] args) {
 
+        String path = "D:\\Users\\Google Drive\\development\\_modules\\03_Advanced\\04_Lab\\Files-and-Streams";
+
+        File file = new File(path);
+
+        if (file.exists()) {
+            if (file.isDirectory()) {
+
+                File[] files = file.listFiles();
+
+                for (File f : files) {
+
+                    if (!f.isDirectory()) {
+                        System.out.printf("%s: [%s]%n", f.getName(), f.length());
+                    }
+                }
+            }
+        }
     }
 }
