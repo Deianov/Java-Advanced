9a2b241dc786322ae10d02b0baff6de6448fbbba
Deianov <deqnow@gmail.com>
2019-05-18 09:40:56 +0200

    Streams, Files and Directories - Lab

---
+++ src/D_StreamsFilesAndDirectories/Lab/L01.java
@@ -0,0 +1,11 @@
+//01.Read File
+package D_StreamsFilesAndDirectories.Lab;
+
+import java.util.Scanner;
+
+class L01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
