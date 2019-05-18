bf2023e9c526f2bf2a65836c45beda98c0d972d0
Deianov <deqnow@gmail.com>
2019-05-18 10:24:09 +0200

    Streams, Files and Directories - Lab

--- src/D_StreamsFilesAndDirectories/Lab/L01.java
+++ src/D_StreamsFilesAndDirectories/Lab/L01.java
@@ -1,11 +1,23 @@
 //01.Read File
 package D_StreamsFilesAndDirectories.Lab;
 
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 class L01 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        String path = "D:\\Users\\Google Drive\\development\\_modules\\03_Advanced\\04_Lab\\";
+        String file = "input.txt";
+
+        try (FileInputStream fileInputStream = new FileInputStream(path + file)) {
+            int oneByte;
+            while ((oneByte = fileInputStream.read()) >= 0) {
+
+                System.out.printf("%s ", Integer.toBinaryString(oneByte));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
