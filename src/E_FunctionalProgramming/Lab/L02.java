f206b2fbdc00c7122aab36cb2f3cf0ad95137551
Deianov <deqnow@gmail.com>
2019-05-20 20:40:11 +0200

    Functional Programming - Lab

--- src/E_FunctionalProgramming/Lab/L02.java
+++ src/E_FunctionalProgramming/Lab/L02.java
@@ -1,7 +1,27 @@
+//02. Sum Numbers
 package E_FunctionalProgramming.Lab;
+
+import java.util.Scanner;
+import java.util.function.Function;
 
 class L02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Function<String, Integer> parser = x->Integer.parseInt(x);
+
+        int count = 0;
+        int sum = 0;
+        String input = scanner.nextLine();
+
+        if (input.length() > 0) {
+            for (String s : input.split(", ")) {
+                count++;
+                sum += parser.apply(s);
+            }
+        }
+
+        System.out.printf("Count = %d%nSum = %d", count, sum);
 
     }
 }
