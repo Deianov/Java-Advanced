29943f14a33f1233f455bc5b131b2d1fb3179d5a
Deianov <deqnow@gmail.com>
2019-05-22 22:47:22 +0200

    Functional Programming - Exercises

---
+++ src/E_FunctionalProgramming/Exercises/E05.java
@@ -0,0 +1,28 @@
+//05. Reverse And Exclude
+package E_FunctionalProgramming.Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class E05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        int divider = Integer.parseInt(scanner.nextLine());
+
+        Predicate<Integer> divisible = number -> number % divider != 0;
+
+        Consumer<int[]> printer = arr -> {
+            for (int i = arr.length-1; i >= 0 ; i--) {
+                if (divisible.test(arr[i])) {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        };
+
+        printer.accept(numbers);
+    }
+}
