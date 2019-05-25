e0e1a3516225ba9a512dcccb188c427450056823
Deianov <deqnow@gmail.com>
2019-05-25 17:46:57 +0200

    Functional Programming - Exercises

--- src/E_FunctionalProgramming/Exercises/E10.java
+++ src/E_FunctionalProgramming/Exercises/E10.java
@@ -1,30 +1,24 @@
-//10. Predicate Party! ??? 80%
+//10. Predicate Party!
 package E_FunctionalProgramming.Exercises;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class E10 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         String line = reader.readLine();
-        if (line.isEmpty()) {
-            System.out.println("Nobody is going to the party!");
-            return;
-        }
 
-        String[] arr = line.split("\\s+");
-        List<String> guests = new ArrayList<>();
-        Collections.addAll(guests, arr);
+        List<String> guests = Arrays.stream(line.split("\\s+")).collect(Collectors.toList());
 
-        while (!"Party!".equalsIgnoreCase(line = reader.readLine())) {
+        while (!"Party!".equals(line = reader.readLine())) {
 
             String[] commandTokens = line.split("\\s+");
 
@@ -32,27 +26,23 @@ class E10 {
             String predicateType = commandTokens[1];
             String predicateArgument = commandTokens[2];
 
-            if (command.equalsIgnoreCase("Remove")) {
-
+            if (command.equals("Remove")) {
                 guests.removeIf(getPredicate(predicateType, predicateArgument));
+            }
+            else if (command.equals("Double")) {
 
-            } else if (command.equalsIgnoreCase("Double")) {
-
-                List<String> additionalGuests = new ArrayList<>();
-
-                for (String guest : guests) {
+                for (int i = 0; i < guests.size(); i++) {
+                    String guest = guests.get(i);
                     if (getPredicate(predicateType, predicateArgument).test(guest)) {
-                        additionalGuests.add(guest);
+                        guests.add(i++, guest);
                     }
-                    additionalGuests.add(guest);
                 }
-                guests = additionalGuests;
             }
         }
-
         if (guests.isEmpty()) {
             System.out.println("Nobody is going to the party!");
         } else {
+            Collections.sort(guests);
             System.out.println(String.format("%s are going to the party!", String.join(", ", guests)));
         }
     }
@@ -60,11 +50,13 @@ class E10 {
     private static Predicate<String> getPredicate(String type, String parameter) {
         switch (type) {
             case "StartsWith":
-                return quest -> quest.startsWith(parameter);
+                return text -> text.startsWith(parameter);
             case "EndsWith":
-                return quest -> quest.endsWith(parameter);
+                return text -> text.endsWith(parameter);
+            case "Length":
+                return text -> text.length() == Integer.parseInt(parameter);
             default:
-                return quest -> quest.length() == Integer.parseInt(parameter);
+                return text -> false;
         }
     }
 }
