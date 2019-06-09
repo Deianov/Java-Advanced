b70a264eae445d5748bad433451cc108bc72a78b
Deianov <deqnow@gmail.com>
2019-06-09 14:14:14 +0200

    Basic Algorithms - Lab

---
+++ src/J_BasicAlgorithms/Lab/RecursiveArraySum.java
@@ -0,0 +1,24 @@
+//01. Recursive Array Sum
+package J_BasicAlgorithms.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RecursiveArraySum {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(sum(array, 0));
+    }
+
+    private static int sum(int[] array, int index) {
+
+        if (index == array.length - 1) {
+            return array[index];
+        }
+        return array[index] + sum(array, index + 1);
+    }
+}
