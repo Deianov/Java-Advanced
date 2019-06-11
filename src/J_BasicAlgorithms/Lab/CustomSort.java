3aed48fc4d320402bc59c060e5db8a25dbdb94c3
Deianov <deqnow@gmail.com>
2019-06-11 21:51:02 +0200

    Basic Algorithms - Lab

---
+++ src/J_BasicAlgorithms/Lab/CustomSort.java
@@ -0,0 +1,39 @@
+// 04. Sorting
+package J_BasicAlgorithms.Lab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class CustomSort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numbers = Arrays.stream(reader.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+
+        shellSort(numbers);
+        for (int i = 0; i < numbers.length; ++i) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+
+    private static void shellSort(int[] numbers) {
+        int length = numbers.length;
+
+        for (int gap = length/2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < length; i += 1) {
+
+                int temp = numbers[i];
+
+                int j;
+                for (j = i; j >= gap && numbers[j - gap] > temp; j -= gap)
+                    numbers[j] = numbers[j - gap];
+
+                numbers[j] = temp;
+            }
+        }
+    }
+}
--- src/J_BasicAlgorithms/Lab/Sorting.java
+++ src/J_BasicAlgorithms/Lab/Sorting.java
@@ -1,34 +1,73 @@
 package J_BasicAlgorithms.Lab;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Random;
 
 class Sorting {
 
     private static int[] array;
     private static int[] numbers;
+    private static long startTime = 0;
+    private static long endTime;
+    private static int RANGE = 10000;
 
     public static void main(String[] args) {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String line;
+        Random random = new Random(System.currentTimeMillis());
 
-        array = new int[] {9, 1, 7, 4, 2, 5, 6, 0, 3, 8};
+        // init array
+        array = new int[RANGE];
+        for (int i = 0; i < RANGE; i++) { array[i] = random.nextInt(RANGE); }
+
         numbers = new int[array.length];
         print();
 
-        bubbleSort();
-        print();
+        startTime = System.nanoTime();
+        bubbleSort(numbers);
+        print("Bubble Sort");
 
+        startTime = System.nanoTime();
+        insertionSort(numbers);
+        print("Insertion Sort");
+
+        startTime = System.nanoTime();
+        shellSort(numbers);
+        print("Shell Sort");
     }
 
-    private static void print() {
-        System.out.println(Arrays.toString(numbers).replaceAll("[\\[\\],]", ""));
-        System.arraycopy(array, 0, numbers, 0, array.length);
+
+
+    private static void shellSort(int[] numbers) {
+        // Start with a big gap, then reduce the gap
+        int length = numbers.length;
+
+        for (int gap = length/2; gap > 0; gap /= 2) {
+            // Do a gapped insertion sort for this gap size.
+            // The first gap elements a[0..gap-1] are already
+            // in gapped order keep adding one more element
+            // until the entire array is gap sorted
+
+            for (int i = gap; i < length; i += 1) {
+                // add a[i] to the elements that have been gap
+                // sorted save a[i] in temp and make a hole at
+                // position i
+                int temp = numbers[i];
+
+                // shift earlier gap-sorted elements up until
+                // the correct location for a[i] is found
+                int j;
+                for (j = i; j >= gap && numbers[j - gap] > temp; j -= gap)
+                    numbers[j] = numbers[j - gap];
+
+                // put temp (the original a[i]) in its correct
+                // location
+                numbers[j] = temp;
+            }
+        }
     }
 
-    private static void bubbleSort() {
+
+
+    private static void bubbleSort(int[] numbers) {
         boolean swap = true;
         int last = numbers.length;
 
@@ -46,5 +85,37 @@ class Sorting {
             }
             last--;
         }
+    }
+
+    private static void insertionSort(int[] numbers) {
+
+        int length = numbers.length;
+
+        for (int i = 1; i < length ; i++) {
+
+            int current = numbers[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position.
+            while (j >= 0 && numbers[j] > current) {
+
+                numbers[j + 1] = numbers[j];
+                j--;
+            }
+            numbers[j + 1] = current;
+        }
+    }
+
+    private static void print(String... name) {
+        endTime = System.nanoTime();
+        if (startTime != 0) {
+            System.out.println(name[0]);
+            System.out.println(String.format("Time: %d milliseconds.", ((endTime - startTime)/1000000)));
+            for (int i = 0; i < RANGE; ++i) {
+                System.out.print(numbers[i] + " ");
+            }
+            System.out.println();
+        }
+        System.arraycopy(array, 0, numbers, 0, array.length);
     }
 }
