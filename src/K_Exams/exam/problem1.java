24f89085020b2d3c5851a1b455724024414ff3bd
Deianov <deqnow@gmail.com>
2019-08-12 17:34:14 +0300

    exam templates

---
+++ src/K_Exams/exam/problem1.java
@@ -0,0 +1,12 @@
+package K_Exams.exam;
+
+import java.util.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
